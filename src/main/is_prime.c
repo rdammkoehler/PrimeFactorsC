0a61d65e561c75b0c4b2b0bcb2a6b8df4137a6ba
Rich Dammkoehler <rpd@noradltd.com>
2013-01-21 18:36:20 -0600

    Rich D: flogged that to death

--- src/main/is_prime.c
+++ src/main/is_prime.c
@@ -3,7 +3,7 @@
 bool isPrime(unsigned int number)
 {
   bool prime = number != 1;
-  for(int i = 2; i < number; i++)
+  for(int i = 2; i <= number/2; i++)
     {
       prime &= number % i != 0;
     }
