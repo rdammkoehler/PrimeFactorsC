960692eab6707e9088387da0a35101b1dc55bcdd
Rich Dammkoehler <rpd@noradltd.com>
2013-01-21 23:55:26 -0600

    Rich D: some steps toward factorization, with debug output

--- src/main/factorizer.c
+++ src/main/factorizer.c
@@ -1,6 +1,22 @@
 #include "factorizer.h"
+#include <math.h>
+#include <stdio.h>
 
 int *factors_of(unsigned int number)
 {
-  return 0;
+  int *factors = malloc(2*sizeof(int));
+  factors[0] = 1;
+  int n = 1;
+  for(int i = 2; i < sqrt(number); i++, n++)
+    {
+      printf("number:%d i:%d\n",number,i);
+      if (number % i == 0)
+	{
+	  printf("%d is a factor of %d",i,number);
+	  factors = realloc(factors, n+1);
+	  factors[n] = i;
+	}
+    }
+  factors[n] = number;
+  return factors;
 }
--- src/test/factorize_tests.c
+++ src/test/factorize_tests.c
@@ -13,8 +13,41 @@ TEST(Factorize,FactorsOfOne)
   TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 1);
 }
 
+TEST(Factorize,FactorsOfTwo)
+{
+  int expected[] = { 1, 2};
+  int *actual = factors_of(2);
+  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 2);
+}
+
+TEST(Factorize,FactorsOfThree)
+{
+  int expected[] = { 1, 3 };
+  int *actual = factors_of(3);
+  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 2);
+}
+
+TEST(Factorize,FactorsOfFour)
+{
+}
+
+TEST(Factorize,FactorsOfFive)
+{
+}
+
+TEST(Factorize,FactorsOfEight)
+{
+}
+
+TEST(Factorize,FactorsOfTwelve)
+{
+}
+
+
 TEST_GROUP_RUNNER(Factorize)
 {
   RUN_TEST_CASE(Factorize,FactorsOfOne);
+  RUN_TEST_CASE(Factorize,FactorsOfTwo);
+  RUN_TEST_CASE(Factorize,FactorsOfThree);
 }
 
