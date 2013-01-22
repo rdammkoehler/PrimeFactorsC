58b5e4c66ccdfd49bc9733b3ef0467addaadef82
Rich Dammkoehler <rpd@noradltd.com>
2013-01-22 02:15:02 -0600

    Rich D: removed old crufty stuff I dont need

--- src/main/prime_factors.c
+++ src/main/prime_factors.c
@@ -1,6 +1,19 @@
 #include "prime_factors.h"
+#include "factorizer.h"
+#include "is_prime.h"
+#include <stdlib.h>
 
 int *prime_factors_of(unsigned int number)
 {
-  return 0;
+  int *prime_factors = (number==1)?NULL:malloc(0);
+  int count = 0;
+  for( int candidate = 2; number > 1; candidate++ )
+    {
+      for(; number % candidate == 0; number /= candidate) {
+	prime_factors = realloc(prime_factors, (count+1)*sizeof(int));
+	prime_factors[count] = candidate;
+	count++;
+      }
+    }
+  return prime_factors;
 }
--- src/test/prime_factors_tests.c
+++ src/test/prime_factors_tests.c
@@ -8,58 +8,57 @@ TEST_TEAR_DOWN(PrimeFactors) {}
 
 TEST(PrimeFactors,FactorsOfOne)
 {
-  int expected[] = { 1 };
   int *actual = prime_factors_of(1);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 1);
+  TEST_ASSERT_NULL(actual);
 }
 
 TEST(PrimeFactors,FactorsOfTwo)
 {
-  int expected[] = { 1, 2};
+  int expected[] = { 2 };
   int *actual = prime_factors_of(2);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 2);
+  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 1);
 }
 
 TEST(PrimeFactors,FactorsOfThree)
 {
-  int expected[] = { 1, 3 };
+  int expected[] = { 3 };
   int *actual = prime_factors_of(3);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 2);
+  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 1);
 }
 
 TEST(PrimeFactors,FactorsOfFour)
 {
-  int expected[] = { 1, 2, 4 };
+  int expected[] = { 2, 2 };
   int *actual = prime_factors_of(4);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 3);
+  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 2);
 }
 
 TEST(PrimeFactors,FactorsOfFive)
 {
-  int expected[] = { 1, 5 };
+  int expected[] = { 5 };
   int *actual = prime_factors_of(5);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 2);
+  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 1);
 }
 
 TEST(PrimeFactors,FactorsOfEight)
 {
-  int expected[] = { 1, 2, 4, 8 };
+  int expected[] = { 2, 2, 2 };
   int *actual = prime_factors_of(8);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 4);
+  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 3);
 }
 
 TEST(PrimeFactors,FactorsOfTweleve)
 {
-  int expected[] = { 1, 2, 3, 4, 6, 12 };
+  int expected[] = { 2, 2, 3 };
   int *actual = prime_factors_of(12);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 6);
+  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 3);
 }
 
 TEST(PrimeFactors,FactorsOfTwentyFour)
 {
-  int expected[] = { 1, 2, 3, 4, 6, 8, 12, 24 };
+  int expected[] = { 2, 2, 2, 3 };
   int *actual = prime_factors_of(24);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 8);
+  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 4);
 }
 
 TEST_GROUP_RUNNER(PrimeFactors)
