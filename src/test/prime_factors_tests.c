d0b1beff429ea6d2c5fa56e09012a54e3532f45a
Rich Dammkoehler <rpd@noradltd.com>
2013-02-15 11:01:07 -0600

    Rich D: decluttered the test code to clarify the experiment

--- src/test/prime_factors_tests.c
+++ src/test/prime_factors_tests.c
@@ -6,21 +6,6 @@ TEST_GROUP(PrimeFactors);
 TEST_SETUP(PrimeFactors) {}
 TEST_TEAR_DOWN(PrimeFactors) {}
 
-
-TEST(PrimeFactors,FactorsOfOne)
-{
-  int *actual = prime_factors_of(1);
-  TEST_ASSERT_NULL(actual);
-}
-
-TEST(PrimeFactors,FactorsOfOne_Alt)
-{
-  int *actual;
-  int count = alt_prime_factors_of(1, &actual);
-  TEST_ASSERT_EQUAL(0,count);
-  TEST_ASSERT_NULL(actual);
-}
-
 TEST(PrimeFactors,FactorsOfTwo)
 {
   int expected[] = { 2 };
@@ -38,119 +23,10 @@ TEST(PrimeFactors, FactorsOfTwo_Alt)
   free(actual);
 }
 
-TEST(PrimeFactors,FactorsOfThree)
-{
-  int expected[] = { 3 };
-  int *actual = prime_factors_of(3);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 1);
-}
-
-TEST(PrimeFactors,FactorsOfThree_Alt)
-{
-  int *actual = NULL;
-  int expected[] = { 3 };
-  int count = alt_prime_factors_of(3, &actual);
-  TEST_ASSERT_EQUAL(1,count);
-  TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, count);
-}
-
-TEST(PrimeFactors,FactorsOfFour)
-{
-  int expected[] = { 2, 2 };
-  int *actual = prime_factors_of(4);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 2);
-}
-
-TEST(PrimeFactors,FactorsOfFour_Alt)
-{
-  int *actual = NULL;
-  int expected[] = { 2, 2 };
-  int count = alt_prime_factors_of(4, &actual);
-  TEST_ASSERT_EQUAL(2,count);
-  TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, count);
-}
-
-TEST(PrimeFactors,FactorsOfFive)
-{
-  int expected[] = { 5 };
-  int *actual = prime_factors_of(5);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 1);
-}
-
-TEST(PrimeFactors,FactorsOfFive_Alt)
-{
-  int *actual = NULL;
-  int expected[] = { 5 };
-  int count = alt_prime_factors_of(5, &actual);
-  TEST_ASSERT_EQUAL(1, count);
-  TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, count);
-}
-
-TEST(PrimeFactors,FactorsOfEight)
-{
-  int expected[] = { 2, 2, 2 };
-  int *actual = prime_factors_of(8);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 3);
-}
-
-TEST(PrimeFactors,FactorsOfEight_Alt)
-{
-  int *actual = NULL;
-  int expected[] = { 2, 2, 2 };
-  int count = alt_prime_factors_of(8, &actual);
-  TEST_ASSERT_EQUAL(3, count);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, count);
-}
-
-TEST(PrimeFactors,FactorsOfTweleve)
-{
-  int expected[] = { 2, 2, 3 };
-  int *actual = prime_factors_of(12);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 3);
-}
-
-TEST(PrimeFactors,FactorsOfTweleve_Alt)
-{
-  int *actual = NULL;
-  int expected[] = { 2, 2, 3 };
-  int count = alt_prime_factors_of(12, &actual);
-  TEST_ASSERT_EQUAL(3, count);
-  TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, count);
-}
-
-TEST(PrimeFactors,FactorsOfTwentyFour)
-{
-  int expected[] = { 2, 2, 2, 3 };
-  int *actual = prime_factors_of(24);
-  TEST_ASSERT_EQUAL_UINT_ARRAY(expected, actual, 4);
-}
-
-TEST(PrimeFactors,FactorsOfTwentyFour_Alt)
-{
-  int *actual = NULL;
-  int expected[] = { 2, 2, 2, 3 };
-  int count = alt_prime_factors_of(24, &actual);
-  TEST_ASSERT_EQUAL(4, count);
-  TEST_ASSERT_EQUAL_INT_ARRAY(expected, actual, count);
-}
 
 TEST_GROUP_RUNNER(PrimeFactors)
 {
-  RUN_TEST_CASE(PrimeFactors,FactorsOfOne);
-  RUN_TEST_CASE(PrimeFactors,FactorsOfOne_Alt);
   RUN_TEST_CASE(PrimeFactors,FactorsOfTwo);
   RUN_TEST_CASE(PrimeFactors,FactorsOfTwo_Alt);
-  RUN_TEST_CASE(PrimeFactors,FactorsOfThree);
-  RUN_TEST_CASE(PrimeFactors,FactorsOfThree_Alt);
-  RUN_TEST_CASE(PrimeFactors,FactorsOfFour);
-  RUN_TEST_CASE(PrimeFactors,FactorsOfFour_Alt);
-  RUN_TEST_CASE(PrimeFactors,FactorsOfFive);
-  RUN_TEST_CASE(PrimeFactors,FactorsOfFive_Alt);
-  RUN_TEST_CASE(PrimeFactors,FactorsOfEight);
-  RUN_TEST_CASE(PrimeFactors,FactorsOfEight_Alt);
-  RUN_TEST_CASE(PrimeFactors,FactorsOfTweleve);
-  RUN_TEST_CASE(PrimeFactors,FactorsOfTweleve_Alt);
-  RUN_TEST_CASE(PrimeFactors,FactorsOfTwentyFour);
-  RUN_TEST_CASE(PrimeFactors,FactorsOfTwentyFour_Alt);
 }
 
